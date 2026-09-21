5b128706c7d6c2d1d11dcc150f6d3d3bc71e1ea0
Amit <amitkatti5@gmail.com>
2026-09-21 17:24:43 +0530

    committ done

---
+++ githubActionsAmit/src/main/java/demopackage/ConflictResolution.java
@@ -0,0 +1,7 @@
+package demopackage;
+
+public class ConflictResolution {
+	public static void main(String[] args) {
+		System.out.println("make chnages below");
+	}
+}
--- githubActionsAmit/src/test/java/loginTest/CreateLogin.java
+++ githubActionsAmit/src/test/java/loginTest/CreateLogin.java
@@ -3,6 +3,7 @@ package loginTest;
 public class CreateLogin {
 	public static void main(String[] args) {
 		System.out.println("conflict check");
+		
 	}
 
 }
